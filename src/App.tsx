c399b240fa8e8ccb6ff8993b27866ce750febddf
Ben Stolovitz <Benjamin.Stolovitz@microsoft.com>
2023-01-11 15:55:10 -0800

    hmmm

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,41 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <>
+      <h1>Porta-TAR calculator</h1>
+      <p>Yo, I heard you like tracking your vacation</p>
+
+      <label>
+        Start date:
+        <input type="month" />
+      </label>
+    </>
+  );
+
+  // return (
+  //   <div className="App">
+  //     <div>
+  //       <a href="https://vitejs.dev" target="_blank">
+  //         <img src="/vite.svg" className="logo" alt="Vite logo" />
+  //       </a>
+  //       <a href="https://reactjs.org" target="_blank">
+  //         <img src={reactLogo} className="logo react" alt="React logo" />
+  //       </a>
+  //     </div>
+  //     <h1>Vite + React</h1>
+  //     <div className="card">
+  //       <button onClick={() => setCount((count) => count + 1)}>
+  //         count is {count}
+  //       </button>
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to test HMR
+  //       </p>
+  //     </div>
+  //     <p className="read-the-docs">
+  //       Click on the Vite and React logos to learn more
+  //     </p>
+  //   </div>
+  // )
 }
 
 export default App
