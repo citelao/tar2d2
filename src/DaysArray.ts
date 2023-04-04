7827e62d717f5494c7ea00370181eb64aea40333
Ben Stolovitz <ben@stolovitz.com>
2023-04-03 22:49:40 -0400

    fix typing

--- src/DaysArray.ts
+++ src/DaysArray.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const DaysArray = z.array(z.object({
     day_iso: z.string(),
     hours: z.number()
-}).optional());
+}));
 
 type IDaysArray = z.infer<typeof DaysArray>;
 
